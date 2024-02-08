ceb90c98b5d5b38843cbdf68d296e6c030f9c201
sunilkumar-deeple <sunilkumardeeple2022@gmail.com>
2024-02-08 13:33:36 +0530

    here multiplication done

--- src/Math.java
+++ src/Math.java
@@ -7,5 +7,13 @@ public class Math {
         int b=10;
         int c=(a+b);
         System.out.println(c);
+
+        //Multiplication
+
+        int x=10;
+        int y=10;
+        int z=(a*b);
+        System.out.println(z);
+
     }
 }
