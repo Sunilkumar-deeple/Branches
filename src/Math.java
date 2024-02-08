efefcc91fbec0e1d272abaa5c54e93a3585734d6
sunilkumar-deeple <sunilkumardeeple2022@gmail.com>
2024-02-08 13:13:26 +0530

    created addition in brances

---
+++ src/Math.java
@@ -0,0 +1,11 @@
+public class Math {
+    public static void main(String[] args) {
+
+
+        //addition
+        int a=10;
+        int b=10;
+        int c=(a+b);
+        System.out.println(c);
+    }
+}
